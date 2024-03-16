3ffecfbe4a9112856140dca15e39ba95b7dcbf80
cmackin9500 <casey@mackinnon.org>
2024-03-16 15:36:10 -0600

    Using Ionico for Add Icon in the tab navigation

--- homeStack.js
+++ homeStack.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import Home from "./screens/home";
 import Goal from "./screens/goals";
@@ -16,7 +17,25 @@ export default function HomeStack({ para }) {
 
   return (
     <NavigationContainer independent={true}>
-      <Tab.Navigator initialRouteName="Home">
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Add") {
+              iconName = focused ? "close" : "add";
+            } else if (route.name === "Settings") {
+              iconName = focused ? "ios-list" : "ios-list-outline";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
         <Tab.Screen
           name="Home"
           component={Home}
